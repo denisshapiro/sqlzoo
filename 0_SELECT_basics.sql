5fcb181720a37f68e7b6f5d9459eaa2b7ba8ea88
denisshapiro <denis.o.shapiro@gmail.com>
2019-07-31 23:44:10 -0500

    Finish exercise 1

--- 0_SELECT_basics.sql
+++ 0_SELECT_basics.sql
@@ -1,14 +1,11 @@
--- 1
-
+-- 1 Query to show the population of 'Germany'
 SELECT population FROM world
   WHERE name = 'Germany'
 
--- 2
-
+-- 2 Query to show the name and population for 'Sweden', 'Norway' and 'Denmark'
 SELECT name, population FROM world
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
--- 3
-
+-- 3 Query to show name and area between 200,000 and 250,000 sq km.
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
